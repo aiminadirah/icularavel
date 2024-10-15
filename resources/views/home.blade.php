7d1f5562f3e8e23e4b2d05e82d16f53606eec120
aimi <aiminadirahlatib@gmail.com>
2024-10-15 10:31:44 +0800

    1. tambah nama app 2. route changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
 
-    <h1>Hai Laravel</h1>
+    <h1>Hai Laravel {{$name ?? "new"}}</h1>
     
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home',function () {
+Route::get('/home2/{name}',function ($name) {
+    return view('home', ['name' => $name]);
+
+});
+
+Route::get('/home',function (){
     return view('home');
 
 });
@@ -14,4 +19,50 @@ Route::get('/home',function () {
 Route::get('/auth/signin',function () {
     return view('auth.signin');
 
+});
+
+// Route Param
+Route::get('/user/{name}/{age}', function ($name, $age) {
+    return 'User '.$name . ' age is:' .$age;
+
+});
+
+// Named Route
+Route::get('/user/profile', function () {
+    return 'Pengguna Profile Baru';
+}) ->name('user.profile');
+
+// Route Param
+Route::get('/user/{name}', function ($name) {
+    return 'User '.$name ;
+
+});
+// alias of a route user.profile = /pengguna/profile
+
+// Redirect route to named route
+Route::get('/redirect-to-profile', function () {
+    return redirect()->route('user.profile');
+});
+
+// Route Group
+Route::prefix('food')->group(function () {
+    
+    Route::get('/details',function () {
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function(){
+        return 'Food home page';
+
+    });
+});
+
+    route::name('job')->prefix('job')->group(function(){
+        route::get('/home', function(){
+            return 'Job home page';
+        })->name('.home');
+    
+        route::get('/description', function(){
+            return 'Job details are following';
+        })->name('.description');
 });
