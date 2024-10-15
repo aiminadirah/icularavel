497dc49867bbdf398f533f869ef264becc319e0c
aimi <aiminadirahlatib@gmail.com>
2024-10-15 12:40:17 +0800

    tambah button, simplekan code,tukar route about dan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') }}</title>
-</head>
-<body>
+@extends('layouts.main')
 
-    <h1>Hai Laravel {{$name ?? "new"}}</h1>
-    
-</body>
-</html>
+@section('title', 'Home')
+
+@section('content')
+
+    @php
+    $_name = $name ?? "team";
+    @endphp 
+
+    @if($_name == "aimi")
+     <p>You're banned</p>
+    @else
+     <h1> Hello, {{ $_name }}</h1>      
+    @endif
+    <button type="button" class="btn btn-primary">Click me</button>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home2/{name}',function ($name) {
+Route::get('/home/{name}',function ($name) {
     return view('home', ['name' => $name]);
 
 });
 
 Route::get('/home',function (){
     return view('home');
+})->name('home');
 
-});
+Route::get('/about',function (){
+    return view('about');
+})->name('about');;
 
 Route::get('/auth/signin',function () {
     return view('auth.signin');
